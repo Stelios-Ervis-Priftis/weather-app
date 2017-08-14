088758d95961ce2b33e6e754af4d8d8a6b07830b
Ervis-Priftis <steliospriftis@gmail.com>
2017-08-14 02:59:14 +0100

    Wiring up Weather Search

--- app.js
+++ app.js
@@ -1,24 +1,46 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode.js');
+//
+// const argv = yargs
+// .options({
+//   a: {
+//     demand: true,
+//     alias: 'address',
+//     describe: 'Address to fetch weather for',
+//     string: true
+//   }
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const geocode = require('./geocode/geocode.js');
+// API key ba224966e43b499a25d5ac55f7a12e4d
 
-const argv = yargs
-.options({
-  a: {
-    demand: true,
-    alias: 'address',
-    describe: 'Address to fetch weather for',
-    string: true
-  }
-})
-.help()
-.alias('help', 'h')
-.argv;
+const request = require('request');
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+request({
+  url: 'https://api.darksky.net/forecast/ba224966e43b499a25d5ac55f7a12e4d/51.6526714,-0.0961436',
+  json: true
+}, (error, response, body) => {
+  // if (error) {
+  //   console.log('Unable to connect to Forecast.io server.');
+  // } else if (response.statusCode === 400) {
+  //   console.log('Unable to fetch weather');
+  // } else if (response.statusCode === 200) {
+  //   console.log(JSON.stringify(body.currently.temperature));
+  // }
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather');
   }
 });
